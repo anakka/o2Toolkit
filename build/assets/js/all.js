ee786befb7fbf3457a5dd5b9fc830c7c0c9b2f2f
Rajesh Mewada <mewadarajesh43@gmail.com>
2016-08-25 15:28:22 +0100

    [NH]Header search removed.

--- build/assets/js/all.js
+++ build/assets/js/all.js
@@ -45571,83 +45571,6 @@ myApp.directive('compile', ['$compile', function ($compile) {
 
 
 
-var myApp=myApp.directive('promoMDirective', function($rootScope,$compile,ModalService,$window) {
-  return {
-      restrict: 'AE',   
-      replace: true,
-      scope:{name:"@"},
-      templateUrl: "./modules/promo-m/template.html",
-		link: function ( $scope, element ) {  
-			var source = angular.copy($rootScope.moduledata);
-			function moduleJson() {
-			  for (var i = 0; i < source.length; i++) {
-			    	if (source[i].name === $scope.name) {
-				      return source[i];
-				    }
-			  }
-			}
-			$scope.module=moduleJson();		
-			$scope.module.titleBol=true; 
-			$scope.module.copyBol=true;
-			$scope.module.ctaBol=true;  
-			$scope.module.shadeClass=$scope.module.extraClass[0].shade;
-			$scope.currentIndex;
-			element.bind('click', function() {  
-				ModalService.showModal({
-		            templateUrl: './modules/common/modaltemplate.html',
-		            controller: "ModalController",
-		            data:$scope.module
-
-		        }).then(function(modal) {
-		        	$scope.resetData = angular.copy($scope.module);
-		            modal.element.show();
-		            modal.close.then(function(result) {	
-		            	if(result==='no') {
-		            		$scope.module=$scope.resetData;
-		            		return;
-		            	}	            	
-		            	responsiveImg();    		            	    
-		            });
-		        });
-
-	        });
-	        function responsiveImg() {	 
-	        	var bgApply=element.find('.bkg-img');
-	        	var responsiveImg=function() {
-	        	if(angular.element($window).width()<575) {
-	        		$(bgApply).css( 'background-image',"url('" + $scope.module.dd + "')"); 
-	        	}
-	        	if(angular.element($window).width()>=575 && angular.element($window).width()<=815) {
-	        		$(bgApply).css( 'background-image',"url('" + $scope.module.bp2 + "')");  
-	        	}
-	        	if(angular.element($window).width()>=815) {
-	        		$(bgApply).css( 'background-image',"url('" + $scope.module.bp3 + "')");
-	        	}
-	        };
-        	responsiveImg();
-        	$($window).on("resize", function () {
-	        	responsiveImg();
-	        });     	
-	        	
-	        	
-	        }
-		}
-  	};
-}); 
-
-myApp.controller('ModalController', function($scope, close,options) {
-	$scope.module=options.data; 
- $scope.close = function(result) {
- 	$scope.module=result;
- 	close(result, 500); // close, but give 500ms for bootstrap to animate
- };
-
-});
-
-
-
-
-
 var myApp=myApp.directive('preCode', function($timeout){
     return {
       restrict: 'AE',
@@ -45726,6 +45649,83 @@ myApp.directive("addmodules", function($compile){
     
   };
 });
+
+var myApp=myApp.directive('promoMDirective', function($rootScope,$compile,ModalService,$window) {
+  return {
+      restrict: 'AE',   
+      replace: true,
+      scope:{name:"@"},
+      templateUrl: "./modules/promo-m/template.html",
+		link: function ( $scope, element ) {  
+			var source = angular.copy($rootScope.moduledata);
+			function moduleJson() {
+			  for (var i = 0; i < source.length; i++) {
+			    	if (source[i].name === $scope.name) {
+				      return source[i];
+				    }
+			  }
+			}
+			$scope.module=moduleJson();		
+			$scope.module.titleBol=true; 
+			$scope.module.copyBol=true;
+			$scope.module.ctaBol=true;  
+			$scope.module.shadeClass=$scope.module.extraClass[0].shade;
+			$scope.currentIndex;
+			element.bind('click', function() {  
+				ModalService.showModal({
+		            templateUrl: './modules/common/modaltemplate.html',
+		            controller: "ModalController",
+		            data:$scope.module
+
+		        }).then(function(modal) {
+		        	$scope.resetData = angular.copy($scope.module);
+		            modal.element.show();
+		            modal.close.then(function(result) {	
+		            	if(result==='no') {
+		            		$scope.module=$scope.resetData;
+		            		return;
+		            	}	            	
+		            	responsiveImg();    		            	    
+		            });
+		        });
+
+	        });
+	        function responsiveImg() {	 
+	        	var bgApply=element.find('.bkg-img');
+	        	var responsiveImg=function() {
+	        	if(angular.element($window).width()<575) {
+	        		$(bgApply).css( 'background-image',"url('" + $scope.module.dd + "')"); 
+	        	}
+	        	if(angular.element($window).width()>=575 && angular.element($window).width()<=815) {
+	        		$(bgApply).css( 'background-image',"url('" + $scope.module.bp2 + "')");  
+	        	}
+	        	if(angular.element($window).width()>=815) {
+	        		$(bgApply).css( 'background-image',"url('" + $scope.module.bp3 + "')");
+	        	}
+	        };
+        	responsiveImg();
+        	$($window).on("resize", function () {
+	        	responsiveImg();
+	        });     	
+	        	
+	        	
+	        }
+		}
+  	};
+}); 
+
+myApp.controller('ModalController', function($scope, close,options) {
+	$scope.module=options.data; 
+ $scope.close = function(result) {
+ 	$scope.module=result;
+ 	close(result, 500); // close, but give 500ms for bootstrap to animate
+ };
+
+});
+
+
+
+
 
 var myApp=myApp.directive('promoSDirective', function($rootScope,$compile,ModalService,$window,modulesService) {
   return {
